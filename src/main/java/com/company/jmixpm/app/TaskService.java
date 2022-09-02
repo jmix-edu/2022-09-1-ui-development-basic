c580ebaafc8ad8ec77ba9b010e9e0ce50a4e0839
Gleb Gorelov <g.gorelov@haulmont.com>
2022-09-02 18:38:40 +0400

    Final

--- src/main/java/com/company/jmixpm/app/TaskService.java
+++ src/main/java/com/company/jmixpm/app/TaskService.java
@@ -1,15 +1,35 @@
 package com.company.jmixpm.app;
 
+import com.company.jmixpm.entity.Project;
+import com.company.jmixpm.entity.Task;
 import com.company.jmixpm.entity.User;
 import io.jmix.core.DataManager;
+import io.jmix.core.security.CurrentAuthentication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class TaskService {
 
     @Autowired
     private DataManager dataManager;
+
+    @Autowired
+    private CurrentAuthentication currentAuthentication;
+
+    public void createTask(Project project, String name, LocalDateTime startDate) {
+        Task task = dataManager.create(Task.class);
+
+        task.setProject(project);
+        task.setName(name);
+        task.setStartDate(startDate);
+        task.setAssignee(((User) currentAuthentication.getUser()));
+
+        dataManager.save(task);
+    }
+
 
     public User findLeastBusyUser() {
         return dataManager.loadValues("select u, count(t.id) " +
--- src/main/java/com/company/jmixpm/screen/main/MainScreen.java
+++ src/main/java/com/company/jmixpm/screen/main/MainScreen.java
@@ -1,18 +1,22 @@
 package com.company.jmixpm.screen.main;
 
+import com.company.jmixpm.app.TaskService;
+import com.company.jmixpm.entity.Project;
+import com.company.jmixpm.entity.Task;
+import io.jmix.ui.Notifications;
+import io.jmix.ui.ScreenBuilders;
 import io.jmix.ui.ScreenTools;
-import io.jmix.ui.component.AppWorkArea;
-import io.jmix.ui.component.Button;
-import io.jmix.ui.component.Window;
+import io.jmix.ui.action.Action;
+import io.jmix.ui.component.*;
 import io.jmix.ui.component.mainwindow.Drawer;
 import io.jmix.ui.icon.JmixIcon;
+import io.jmix.ui.model.CollectionContainer;
+import io.jmix.ui.model.CollectionLoader;
 import io.jmix.ui.navigation.Route;
-import io.jmix.ui.screen.Screen;
-import io.jmix.ui.screen.Subscribe;
-import io.jmix.ui.screen.UiController;
-import io.jmix.ui.screen.UiControllerUtils;
-import io.jmix.ui.screen.UiDescriptor;
+import io.jmix.ui.screen.*;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDateTime;
 
 @UiController("MainScreen")
 @UiDescriptor("main-screen.xml")
@@ -28,6 +32,25 @@ public class MainScreen extends Screen implements Window.HasWorkArea {
     private Drawer drawer;
     @Autowired
     private Button collapseDrawerButton;
+
+    @Autowired
+    private CollectionLoader<Project> projectsDl;
+    @Autowired
+    private CollectionLoader<Task> tasksDl;
+
+    @Autowired
+    private TaskService taskService;
+
+    @Autowired
+    private EntityComboBox<Project> projectSelector;
+    @Autowired
+    private TextField<String> nameSelector;
+    @Autowired
+    private DateField<LocalDateTime> dateSelector;
+    @Autowired
+    private Notifications notifications;
+    @Autowired
+    private MessageBundle messageBundle;
 
     @Override
     public AppWorkArea getWorkArea() {
@@ -50,5 +73,65 @@ public class MainScreen extends Screen implements Window.HasWorkArea {
                 UiControllerUtils.getScreenContext(this).getScreens());
 
         screenTools.handleRedirect();
+    }
+
+    @Subscribe("addTask")
+    public void onAddTask(Action.ActionPerformedEvent event) {
+        if (projectSelector.getValue() == null
+                || nameSelector.getValue() == null
+                || dateSelector.getValue() == null) {
+            notifications.create()
+                    .withCaption(messageBundle.getMessage("validation.fieldsNotFilled.message"))
+                    .withType(Notifications.NotificationType.WARNING)
+                    .show();
+
+            projectSelector.focus();
+            return;
+        }
+
+        taskService.createTask(projectSelector.getValue(),
+                nameSelector.getValue(),
+                dateSelector.getValue());
+
+        tasksDl.load();
+
+        projectSelector.setValue(null);
+        nameSelector.setValue(null);
+        dateSelector.setValue(null);
+    }
+
+    @Autowired
+    private ScreenBuilders screenBuilders;
+
+    @Autowired
+    private CollectionContainer<Task> tasksDc;
+
+    @Subscribe("taskCalendar")
+    public void onTaskCalendarCalendarEventClick(Calendar.CalendarEventClickEvent<Task> event) {
+        Task task = ((Task) event.getEntity());
+        if (task == null) {
+            return;
+        }
+
+        StandardEditor<Task> screen = ((StandardEditor<Task>) screenBuilders.editor(Task.class, this)
+                .editEntity(task)
+                .withOpenMode(OpenMode.DIALOG)
+                .build());
+
+        screen.addAfterCloseListener(afterCloseEvent -> {
+            if (afterCloseEvent.closedWith(StandardOutcome.COMMIT)) {
+//                Task editedEntity = screen.getEditedEntity();
+//                tasksDc.replaceItem(editedEntity);
+                tasksDl.load();
+            }
+        });
+
+        screen.show();
+    }
+
+    @Subscribe("refresh")
+    public void onRefresh(Action.ActionPerformedEvent event) {
+        projectsDl.load();
+        tasksDl.load();
     }
 }
